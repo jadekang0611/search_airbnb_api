e27b8e54e2f6276414ffd4ef04c31833089c724c
daisyy125 <daisy125@gmail.com>
2020-11-17 23:23:22 -0500

    update with get documents based on req queries endpoint

--- routes/properties.js
+++ routes/properties.js
@@ -14,4 +14,29 @@ router.get('/', async (req, res, next) => {
   }
 });
 
+// GET properties based on filters
+
+router.get('/search', async (req, res, next) => {
+  const filters = {...req.query};
+
+  try {
+    const listingAndReviews = await Property.find(filters).limit(5);
+      if (listingAndReviews.length === 0) {
+        return res.status(404).json({
+        status: 'failure',
+        message: `No properties are found with ${filters}`
+      });
+    }
+    console.log(filters)
+    res.status(200).json(listingAndReviews);
+
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: e });
+  }
+});
+
+// PUT update/remove property in category list 
+
+
 module.exports = router;
